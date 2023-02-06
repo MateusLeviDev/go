ae03a9249b218b7225e4c7a9db0d1063726724b3
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-02-06 16:57:17 -0300

    [middleware AppError]

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -8,6 +9,22 @@ app.use(cors());
 app.use(express.json()); //não interpreta json por padrão
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statudCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+    //verificar se o erro é uma instância da clase AppError
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!'); //vai chamar o servidor
