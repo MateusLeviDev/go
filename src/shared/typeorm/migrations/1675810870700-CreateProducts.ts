726ae046aef59acf1be75e9206a29cebf5f68961
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-02-07 21:59:08 -0300

    [create Table Products]

--- src/shared/typeorm/migrations/1675810870700-CreateProducts.ts
+++ src/shared/typeorm/migrations/1675810870700-CreateProducts.ts
@@ -1,11 +1,11 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, Tree } from 'typeorm';
 
 export class CreateProducts1675810870700 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
         name: 'products',
-        collumns: [
+        columns: [
           {
             name: 'id',
             type: 'uuid',
@@ -13,10 +13,36 @@ export class CreateProducts1675810870700 implements MigrationInterface {
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
 }
