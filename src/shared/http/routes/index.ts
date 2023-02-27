a69bf7cc6262659ad4b8b2044c614b7f2a671913
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-02-27 17:32:50 -0300

    add routes.use('/users', userRouter)

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 // arquivo principal de rotas(routes)
 import productsRouter from '@modules/products/routes/products.routes';
+import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
