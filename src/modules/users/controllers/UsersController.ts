a98e73d161446713608449c647f7a418011a7097
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-02-27 16:36:49 -0300

    User controller

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUserService from '../services/ListUserService';
+
+export default class UsersController {
+  //teremos dois métodos [index: usar o serviço de listagem & create: create user]
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
