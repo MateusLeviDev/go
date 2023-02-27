183d30143d7f07934b961e705abe232ad7d3006b
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-02-27 19:19:34 -0300

    hashedPassword

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UserRepository';
@@ -19,10 +20,12 @@ class CreateUserService {
       throw new AppError('Emaill address already used.');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
