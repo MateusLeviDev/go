b566a1fd61e19f9d3fa17bf2b9bab25e9c603fa9
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-03-01 15:46:14 -0300

    import rename

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,8 @@
 // arquivo principal de rotas(routes)
+import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
-import { Router } from 'express';
 
 const routes = Router();
 
