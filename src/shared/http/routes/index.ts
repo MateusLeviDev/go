36d2a78264579474104e18c12a7382321913d892
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-03-01 15:38:06 -0300

    import [routes.use('/sessions', sessionsRouter);]

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 // arquivo principal de rotas(routes)
 import productsRouter from '@modules/products/routes/products.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
@@ -7,5 +8,6 @@ const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
