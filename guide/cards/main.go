074ba5eec30ae5b54c958b80a7ca27f1941b80aa
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-10-07 21:02:37 -0300

    feat: add initial go classes again

---
+++ guide/cards/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
