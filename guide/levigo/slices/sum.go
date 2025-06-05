7c8e7f5d9698360287d5fee537ddaee225210d10
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2025-06-04 22:41:03 -0300

    feat: added Go arrays and slices pt.2 exercises/ and TDD practice

--- guide/levigo/slices/sum.go
+++ guide/levigo/slices/sum.go
@@ -7,3 +7,36 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
+
+func Filter(numbers []int, fn func(int) bool) []int {
+	seen := make(map[int]bool)
+	result := make([]int, 0)
+	for _, number := range numbers {
+		if fn(number) && !seen[number] {
+			seen[number] = true
+			result = append(result, number)
+		}
+	}
+	return result
+}
--- guide/levigo/slices/sum_test.go
+++ guide/levigo/slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -22,5 +25,51 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func checkSums(t testing.TB, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum slices with empty, single and multiple items", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1}, []int{10, 20, 30})
+		want := []int{0, 0, 50}
+		checkSums(t, got, want)
+	})
+}
+
+func TestFilter(t *testing.T) {
+	isEven := func(n int) bool { return n%2 == 0 }
+
+	t.Run("safely filter", func(t *testing.T) {
+		got := Filter([]int{1, 2, 3, 4, 5, 6}, isEven)
+		want := []int{2, 4, 6}
+		checkSums(t, got, want)
+	})
+
+	t.Run("empty slice", func(t *testing.T) {
+		got := Filter([]int{}, isEven)
+		want := []int{}
+		checkSums(t, got, want)
 	})
 }
