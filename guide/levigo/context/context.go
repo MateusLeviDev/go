d81eeaaea119646d7f6536bb7f907b74562925d2
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2025-07-10 18:08:09 -0300

    feat: concurrency #4 -context practices

---
+++ guide/levigo/context/context.go
@@ -0,0 +1,23 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctc context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: log error however you like
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
